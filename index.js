d92bd94469b6cac8cac93834b68bc1de21673099
SEHAINVIXO <Sebastian.haug@invixo.com>
2023-05-31 11:23:29 +0200

    Debugging getOrgInformation

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ async function tryGetColumnAndCardInformation(columnName, projectUrl, token, iss
         var orgLogin = splitUrl[4];
         console.log(`This project is configured at the org level. Org Login:${orgLogin}, project number#${projectNumber}`);
         var orgInformation = await getOrgInformation(orgLogin, projectNumber, token);
+        console.log(orgInformation);
         orgInformation.organization.project.columns.nodes.forEach(function(columnNode){
             var name = columnNode.name;
             if(name == columnName){
